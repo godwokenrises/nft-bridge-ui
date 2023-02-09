fa4174e2d55c71500a389f685e737dc34eef7dc8
ShookLyngs <shooklyngs@gmail.com>
2023-02-09 15:28:08 +0800

    feat: getL2Nrc721TokenId method

--- src/modules/Nrc721/methods/bridge.ts
+++ src/modules/Nrc721/methods/bridge.ts
@@ -1,5 +1,5 @@
 import { Buffer } from "buffer";
-import { utils } from "@ckb-lumos/lumos";
+import { BI, utils } from "@ckb-lumos/lumos";
 import { Hash, Cell, HexString } from "@ckb-lumos/base";
 import { cloneDeep } from "lodash";
 
@@ -21,3 +21,13 @@ export function addNrc721BridgeExtraToNftCell(nftCell: Cell, ethAddress: HexStri
   cell.data = `${cell.data}${header.slice(2)}${ethAddress.slice(2)}`;
   return cell;
 }
+
+export function getL2Nrc721TokenId(nftCellTypeArgs: HexString) {
+  const hasher = new utils.CKBHasher();
+  hasher.update(nftCellTypeArgs);
+
+  const tokenId = hasher.digestHex().slice(0, 42);
+  return BI.from(tokenId);
+}
+
+// console.log("getL2Nrc721TokenId", getL2Nrc721TokenId("0x00000000000000000000000000000000000000000000000000545950455f494401b0d569a664c17a6101b6b8af81ff98f781d87b0a7b016c0107d617bc022b42f7ae47fa2a9235408928eae5df1c6866f6adbc46cf63a8ca09be5481d1b72ef647").toString());
